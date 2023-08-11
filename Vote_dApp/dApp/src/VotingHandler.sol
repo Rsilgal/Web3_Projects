6b8c911716fc244a1049f3b87f17d2c3bdcc240f
Ramon Silvente Galera <rsilgal@gmail.com>
2023-08-11 11:52:59 +0200

    Add first version of contract without events

--- Vote_dApp/dApp/src/VotingHandler.sol
+++ Vote_dApp/dApp/src/VotingHandler.sol
@@ -6,4 +6,35 @@ import {Ownable} from "openzeppelin/access/Ownable.sol";
 
 contract VotingHandler is Ownable {
     Voting[] public votes;
+    mapping(address => uint256[]) creatorOfTheVote;
+    mapping(uint256 => address) identifierFromVote;
+    uint256 totalVotesAdded = 0;
+
+
+    function addVote(
+        uint256 _startDate, 
+        uint256 _durationDays,
+        uint256 _minumumParticipation,
+        address[] memory _options,
+        address[] memory _users
+        ) external {
+        Voting vote = new Voting(_startDate, _durationDays, _minumumParticipation, _options, _users);
+        votes.push(vote);
+        totalVotesAdded++;
+        creatorOfTheVote[msg.sender].push(totalVotesAdded);
+        identifierFromVote[totalVotesAdded] = address(vote);
+        //TODO: Emit an event to send the address of the created contract
+    }
+
+    function getVotesByCreator(address creator) external view returns(address[] memory result) {       
+        for (uint256 i = 0; i < creatorOfTheVote[creator].length; i++) {
+            result[i] = identifierFromVote[creatorOfTheVote[creator][i]];
+        }
+    }
+
+    function getAllVotes() external view returns(address[] memory result){
+        for (uint256 i = 0; i < totalVotesAdded; i++) {
+            result[i] = identifierFromVote[i];
+        }
+    }
 }
